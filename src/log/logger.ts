1c526570f36cf32da61b2003fef7e794524f725c
macedo-erick <macedo.eriick@gmail.com>
2022-11-23 19:10:39 -0300

    Refactor

---
+++ src/log/logger.ts
@@ -0,0 +1,18 @@
+import { createLogger, format, transports, config } from "winston";
+import { S3StreamLogger } from "s3-streamlogger";
+const { combine, timestamp, json } = format;
+
+const s3stream = new S3StreamLogger({
+  bucket: process.env.API_AWS_S3_BUCKET,
+  access_key_id: process.env.API_AWS_ACCESS_KEY,
+  secret_access_key: process.env.API_AWS_SECRET_ACCESS_KEY,
+});
+
+const chargeLogger = createLogger({
+  levels: config.syslog.levels,
+  defaultMeta: { component: "charge-service" },
+  transports: [new transports.Stream({ stream: s3stream }), new transports.Console()],
+  format: combine(timestamp({ format: "YYYY-MM-dd HH:mm:ss" }), json()),
+});
+
+export { chargeLogger };
